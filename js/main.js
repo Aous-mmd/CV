bdbb7a729e2ab671496a898f5a9f446de656b680
Aous <aous.mohammad97@gmal.com>
2020-06-26 13:01:09 -0400

    transition

---
+++ js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+    $("#img1").animate({
+        opacity: 1,
+        top: '10%',
+        left: '50%',
+    }, 3000);
+    $("#img2").animate({
+        opacity: 1,
+        top: '10%',
+        left: '25%',
+    }, 3000);
+    $("#img3").animate({
+        opacity: 1,
+        top: '30%',
+    }, 3000);
+    $("#img4").animate({
+        opacity: 1,
+        top: '50%',
+        right: '60%',
+    }, 3000);
+    $("#img5").animate({
+        opacity: 1,
+        bottom: '30%',
+        right: '68%'
+    }, 3000);
+    $("#img6").animate({
+        opacity: 1,
+        right: '16%'
+    }, 3000);
+    $("#img7").animate({
+        opacity: 1,
+        right: '59%',
+    }, 3000);
+    $("#img8").animate({
+        opacity: 1,
+        right: '15%'
+    }, 3000);
+    $("#img9").animate({
+        opacity: 1,
+        right: '95%'
+    }, 3000);
+    $("#img10").animate({
+        opacity: 1,
+        right: '85%'
+    }, 3000);
+    $("#img11").animate({
+        opacity: 1,
+        bottom: '20%',
+    }, 3000);
+    $("#img12").animate({
+        opacity: 1,
+        bottom: '22%',
+    }, 3000);
+    $("#img13").animate({
+        opacity: 1,
+        bottom: '22%',
+    }, 3000);
+    $("#img14").animate({
+        opacity: 1,
+        bottom: '40%',
+    }, 3000);
+    $("#img15").animate({
+        opacity: 1,
+        bottom: '40%',
+    }, 3000);
+    $("#img16").animate({
+        opacity: 1,
+        left: '50%'
+    }, 3000);
+    $("#img17").animate({
+        opacity: 1,
+        left: '73%',
+    }, 3000);
+    $("#img18").animate({
+        opacity: 1,
+        right: '73%'
+    }, 3000);
+    $("#img19").animate({
+        opacity: 1,
+        left: '85%'
+    }, 3000);
+    $("#img21").animate({
+        opacity: 1,
+        left: '16%'
+    }, 3000);
+    $("#img22").animate({
+        opacity: 1,
+        left: '15%'
+    }, 3000);
+    $("#img23").animate({
+        opacity: 1,
+        left: '1%'
+    }, 3000);
+});
